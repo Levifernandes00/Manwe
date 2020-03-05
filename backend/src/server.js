af9860f068d60f7d6ede01bdba51d673b673a92a
Levi <levifernandes@usp.br>
2020-03-04 21:03:37 -0300

    inital configuration on backend

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const server = express();
+
+mongoose.connect('', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(morgan("dev"));
+
+server.use(require('./routes'));
+
+server.listen(3333);
